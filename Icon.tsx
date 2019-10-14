ecbf57838b5d2d551c672237b0282957dd9b966d
聆一 <fenfen.wff@alibaba-inc.com>
2019-10-14 19:55:26 +0800

    fix: svg icon触发不了事件问题

--- Icon.tsx
+++ Icon.tsx
@@ -3,7 +3,7 @@
  * @description 即支持字体icon，也支持彩色的svg版本
  * @author 2018.04.26
  */
-import * as React from "react";
+import * as React from 'react';
 import * as classNames from 'classnames';
 
 interface IconProps {
@@ -25,26 +25,16 @@ export class Icon extends React.Component<IconProps, any> {
       // type的写法可能为'one-icon icon-***'，正则匹配出icon-***
       const match = type.match(/icon-[^ ]*/);
       iconType = match ? match[0] : type;
-      const myClassNames=classNames("svg-icon", className)
+      const myClassNames = classNames('svg-icon', className);
       return (
-        <svg
-          className={myClassNames}
-          style={style}
-          aria-hidden="true"
-        >
+        <svg className={myClassNames} style={style} {...restProps} aria-hidden="true">
           <use xlinkHref={`#${iconType}`} />
         </svg>
       );
     } else {
-      const myClassNames=classNames("one-icon", className, iconType)
+      const myClassNames = classNames('one-icon', className, iconType);
 
-      return (
-        <i
-          className={myClassNames}
-          style={style}
-          {...restProps}
-        />
-      );
+      return <i className={myClassNames} style={style} {...restProps} />;
     }
   }
 }
